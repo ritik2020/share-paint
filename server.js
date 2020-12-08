b862f5d011a6c97840766116216b36fcfca0c042
Rohan Jha <yorohan.jha@gmail.com>
2020-12-08 13:31:18 +0530

    login and signup added

--- server.js
+++ server.js
@@ -4,17 +4,50 @@ const app = express();
 const http = require('http').createServer(app);
 const socketEvents = require('./socket/socket_main').socketEvents(http);
 
-app.set("view engine", "ejs");
+const mongoose = require('mongoose');
+const router = express.Router();
+const expressEjsLayout = require('express-ejs-layouts')
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require("passport");
+
+
+//EJS
+app.set('view engine','ejs');
+app.use(expressEjsLayout);
+//BodyParser
+app.use(express.urlencoded({extended : false}));
 
 app.use(express.static("./public"));
 
-app.get("/",(req,res)=>{
-  res.render("index");
-});
+require('./config/passport')(passport)
+//mongoose
+mongoose.connect('mongodb+srv://rohan:rohan123@share-paint.j1fni.mongodb.net/share-paint?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log('connected,,'))
+.catch((err)=> console.log(err));
 
-app.get('/paint', (req, res) => {
-    res.render("paint");
-});
+
+//express session
+app.use(session({
+  secret : 'secret',
+  resave : true,
+  saveUninitialized : true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req,res,next)=> {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error  = req.flash('error');
+  next();
+  })
+  
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
+
+
 
 
 
